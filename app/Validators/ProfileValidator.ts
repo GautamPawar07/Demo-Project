33e65767588306b6ab2b4aaad910fe65e4ec5829
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-05-12 01:14:58 +0530

    refactored validators files

--- app/Validators/ProfileValidator.ts
+++ app/Validators/ProfileValidator.ts
@@ -1,28 +1,9 @@
 import { schema, CustomMessages, rules } from "@ioc:Adonis/Core/Validator";
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
-export default class ProfileValidator {
+class ProfileValidator {
   constructor(protected ctx: HttpContextContract) {}
 
-  /*
-   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-   *
-   * For example:
-   * 1. The username must be of data type string. But then also, it should
-   *    not contain special characters or numbers.
-   *    ```
-   *     schema.string({}, [ rules.alpha() ])
-   *    ```
-   *
-   * 2. The email must be of data type string, formatted as a valid
-   *    email. But also, not used by any other user.
-   *    ```
-   *     schema.string({}, [
-   *       rules.email(),
-   *       rules.unique({ table: 'users', column: 'email' }),
-   *     ])
-   *    ```
-   */
   public schema = schema.create({
     name: schema.string({}, [rules.minLength(3), rules.maxLength(30)]),
 
@@ -40,17 +21,6 @@ export default class ProfileValidator {
     }),
   });
 
-  /**
-   * Custom messages for validation failures. You can make use of dot notation `(.)`
-   * for targeting nested fields and array expressions `(*)` for targeting all
-   * children of an array. For example:
-   *
-   * {
-   *   'profile.username.required': 'Username is required',
-   *   'scores.*.number': 'Define scores as valid numbers'
-   * }
-   *
-   */
   public messages: CustomMessages = {
     "name.minLength": "Name must contain minimum 3 characters",
 
@@ -66,6 +36,7 @@ export default class ProfileValidator {
 
     "gender.required": "gender in required",
 
-    "dateOfBirth.date": "date must be in  valid date format",
+    "dateOfBirth.date": "date must be in  valid date format i.e : dd/MM/yyyy",
   };
 }
+export default ProfileValidator;
--- app/Validators/UserValidator.ts
+++ app/Validators/UserValidator.ts
@@ -1,28 +1,9 @@
 import { schema, CustomMessages, rules } from "@ioc:Adonis/Core/Validator";
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
-export default class UserValidator {
+class UserValidator {
   constructor(protected ctx: HttpContextContract) {}
 
-  /*
-   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-   *
-   * For example:
-   * 1. The username must be of data type string. But then also, it should
-   *    not contain special characters or numbers.
-   *    ```
-   *     schema.string({}, [ rules.alpha() ])
-   *    ```
-   *
-   * 2. The email must be of data type string, formatted as a valid
-   *    email. But also, not used by any other user.
-   *    ```
-   *     schema.string({}, [
-   *       rules.email(),
-   *       rules.unique({ table: 'users', column: 'email' }),
-   *     ])
-   *    ```
-   */
   public schema = schema.create({
     email: schema.string({}, [rules.required(), rules.email()]),
 
@@ -33,26 +14,17 @@ export default class UserValidator {
     ]),
   });
 
-  /**
-   * Custom messages for validation failures. You can make use of dot notation `(.)`
-   * for targeting nested fields and array expressions `(*)` for targeting all
-   * children of an array. For example:
-   *
-   * {
-   *   'profile.username.required': 'Username is required',
-   *   'scores.*.number': 'Define scores as valid numbers'
-   * }
-   *
-   */
   public messages: CustomMessages = {
-    "email.required": "email is required",
+    "email.required": "email address is required",
 
-    "email.email": "Invalid email",
+    "email.email": "Invalid email address",
 
-    "password.minLength": "password must contain minimum 3 characters",
+    "password.minLength": "password must contain minimum 8 characters",
 
-    "password.maxLength": "password must contain maximum 30 characters",
+    "password.maxLength": "password must contain maximum 16 characters",
 
     "password.regex": "password must contain AlphaNumeric characters",
   };
 }
+
+export default UserValidator;
