7c27eadf03a7fdd59fe1671dc2c92bb057dc5f65
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-05-12 01:15:41 +0530

    added home route and refacored file

--- start/routes.ts
+++ start/routes.ts
@@ -1,35 +1,17 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from "@ioc:Adonis/Core/Route";
+
+Route.get("/", async () => {
+  return "Welcome to Demo API";
+});
 
 Route.post("/register", "AuthController.register");
 
 Route.post("/login", "AuthController.login");
 
 Route.group(() => {
-  // user
   Route.post("/logout", "AuthController.logout");
 
   Route.group(() => {
-    // profile
     Route.post("/", "ProfilesController.createProfile");
     Route.delete("/", "ProfilesController.deleteProfile");
     Route.put("/", "ProfilesController.updateProfile");
