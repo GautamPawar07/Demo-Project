02b3113af7532ea594eb3d48b73b4db60290ee4d
Gautam-pawar <gautam.pawar.23.2@mountblue.tech>
2023-05-13 11:12:32 +0530

    updated Profile validator with changes as per new userProfile model

--- app/Validators/ProfileValidator.ts
+++ app/Validators/ProfileValidator.ts
@@ -5,7 +5,7 @@ class ProfileValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    name: schema.string({}, [rules.minLength(3), rules.maxLength(30)]),
+    name: schema.string({trim:true}, [rules.minLength(3), rules.maxLength(30)]),
 
     mobile: schema.string({}, [
       rules.required(),
